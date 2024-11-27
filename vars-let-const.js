6926eed821d7062e6c7e49c7e0965a253bd5a270
Danniel Navas <dannielnavas@gmail.com>
2024-11-27 07:30:56 -0500

    first commit

---
+++ vars-let-const.js
@@ -0,0 +1,11 @@
+// Anterior a ES6
+var name = "Daniel";
+
+// Javascript moderno
+let age = 34; // Se puede reasignar
+const PI = 3.14159; // No se puede reasignar
+
+// se recomienda usar const y let en lugar de var
+
+// Var es de alcance global
+// Let y const son de alcance de bloque (solo se pueden usar dentro de un bloque de código)
