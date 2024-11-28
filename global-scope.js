e0b14748bc10f724260476b3111bd07364ffa6f6
Danniel Navas <dannielnavas@gmail.com>
2024-11-28 07:36:23 -0500

    Añadir ejemplo de ámbito global en global-scope.js

---
+++ global-scope.js
@@ -0,0 +1,10 @@
+// ámbito = scope = alcance de una variable
+
+// Ámbito global
+let greeting = "Hola mundo";
+
+function greet() {
+  console.log(greeting);
+}
+
+greet(); // >> imprime "Hola mundo" porque su alcance es global
