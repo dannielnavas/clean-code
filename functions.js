8c8d42954aa4734077ccfa3ae08072106522d24a
Danniel Navas <dannielnavas@gmail.com>
2024-11-28 08:01:07 -0500

    Añadir función getResult que devuelve un resultado fijo

---
+++ functions.js
@@ -0,0 +1,18 @@
+"use strict";
+// Se sabe que se ha desarrollado un código limpio cuando cada función hace exactamente lo que su nombre indica.
+
+// Declaración de funciones
+
+function getResult() {
+  return "Resultado";
+}
+
+getResult(); // >> "Resultado"
+
+// Expresión de funciones
+
+const getResult = function () {
+  return "Resultado";
+};
+
+getResult(); // >> "Resultado"
