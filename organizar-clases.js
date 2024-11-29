dce35e5fca23404563911e1e360f61d6b1d970f6
Danniel Navas <dannielnavas@gmail.com>
2024-11-29 07:56:54 -0500

    Añadir estructura y organización de clases en JavaScript

---
+++ organizar-clases.js
@@ -0,0 +1,6 @@
+// Variables
+// Constantes
+// Variables estáticas
+// Métodos ordenados de mayor importancia a menor importancia la funcionalidad base lo primero y luego los que complementan
+// Funciones estáticas
+// Getters y Setters no se recomienda el acceso directo a las variables
