9d7045dea7ed808e661eb904ad66762fe8b46888
Danniel Navas <dannielnavas@gmail.com>
2024-12-03 07:16:20 -0500

    Añadir directrices sobre la calidad de los comentarios en el código

---
+++ comentarios.js
@@ -0,0 +1,22 @@
+// No comentes código mal escrito, reescribelo
+// uncle bob dice que no hay que comentar el código, sino hacerlo legible
+
+// No explicar el que
+// No explicar el como
+// debe explicar el porque ?
+
+// ❌ El ¿Qué? ⇒ porque el código debe ser Autodescriptivo
+// ❌ El ¿Cómo? ⇒ el código debe ser Autoexplicativo
+// ✅ Sino el ¿Por qué? // Comentar explicando los Criterios, aportando valor
+
+// ❌ Comentario de mala calidad
+// // Suma dos números
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// ✅ Comentario de buena calidad
+// // Suma dos números para calcular el total de ventas
+// function sum(a, b) {
+//     return a + b;
+// }
