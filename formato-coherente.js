1c2413a1a66791d6a746b047c499379091169b8e
Danniel Navas <dannielnavas@gmail.com>
2024-12-03 07:19:42 -0500

    Añadir directrices sobre formato y consistencia en el código

---
+++ formato-coherente.js
@@ -0,0 +1,6 @@
+// Problemas similares, soluciones similares
+// Densidad, apertura y distancia vertical
+
+// Lo mas importante va primero de ese método se llaman los demás que complementan
+
+// identación no importa si es con tabulador o con espacio, lo importante es que sea consistente en todo el proyecto
